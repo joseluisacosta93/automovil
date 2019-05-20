29bdf580b5fa68b9cd1099abcaad16c309a98d3f
jose acosta <joseluisacosta93@gmail.com>
2019-05-20 02:18:24 -0400

    add sql create table

--- automovil.sql
+++ automovil.sql
@@ -1,20 +1,3 @@
--- --------------------------------------------------------
--- Host:                         localhost
--- Versión del servidor:         5.7.24 - MySQL Community Server (GPL)
--- SO del servidor:              Win64
--- HeidiSQL Versión:             9.5.0.5332
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- Volcando estructura de base de datos para automovil
-CREATE DATABASE IF NOT EXISTS `automovil` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `automovil`;
 
 -- Volcando estructura para tabla automovil.clients
 CREATE TABLE IF NOT EXISTS `clients` (
